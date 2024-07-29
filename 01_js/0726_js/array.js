dd51206d9157945e5c45a5571630787241dd6b7a
seon022 <seon022@icloud.com>
2024-07-29 16:29:30 +0900

    0729 제로초자바스크립트

---
+++ 01_js/0726_js/array.js
@@ -0,0 +1,9 @@
+const twoDimension = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+for (let i = 0; i < twoDimension.length; i++) {
+  for (let j = 0; j < twoDimension[i].length; j++) {
+    console.log(twoDimension[i][j]);
+  }
+}
